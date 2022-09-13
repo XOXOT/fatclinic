92cab87f23a8a157de146c57b58feb9cee5a53aa
HeeTaeChoi <53458059+XOXOT@users.noreply.github.com>
2022-09-13 12:44:59 +0900

    Update data.sql

--- src/main/resources/db/mysql/data.sql
+++ src/main/resources/db/mysql/data.sql
@@ -1,13 +1,13 @@
-INSERT IGNORE INTO vets VALUES (1, 'James', 'Carter');
-INSERT IGNORE INTO vets VALUES (2, 'Helen', 'Leary');
-INSERT IGNORE INTO vets VALUES (3, 'Linda', 'Douglas');
-INSERT IGNORE INTO vets VALUES (4, 'Rafael', 'Ortega');
-INSERT IGNORE INTO vets VALUES (5, 'Henry', 'Stevens');
-INSERT IGNORE INTO vets VALUES (6, 'Sharon', 'Jenkins');
+INSERT IGNORE INTO vets VALUES (1, 'fat', 'clinic');
+INSERT IGNORE INTO vets VALUES (2, 'fat', 'clinic');
+INSERT IGNORE INTO vets VALUES (3, 'fat', 'clinic');
+INSERT IGNORE INTO vets VALUES (4, 'fat', 'clinic');
+INSERT IGNORE INTO vets VALUES (5, 'fat', 'clinic');
+INSERT IGNORE INTO vets VALUES (6, 'fat', 'clinic');
 
-INSERT IGNORE INTO specialties VALUES (1, 'radiology');
-INSERT IGNORE INTO specialties VALUES (2, 'surgery');
-INSERT IGNORE INTO specialties VALUES (3, 'dentistry');
+INSERT IGNORE INTO specialties VALUES (1, '서울');
+INSERT IGNORE INTO specialties VALUES (2, '부산');
+INSERT IGNORE INTO specialties VALUES (3, '수원');
 
 INSERT IGNORE INTO vet_specialties VALUES (2, 1);
 INSERT IGNORE INTO vet_specialties VALUES (3, 2);
@@ -15,12 +15,12 @@ INSERT IGNORE INTO vet_specialties VALUES (3, 3);
 INSERT IGNORE INTO vet_specialties VALUES (4, 2);
 INSERT IGNORE INTO vet_specialties VALUES (5, 1);
 
-INSERT IGNORE INTO types VALUES (1, 'cat');
-INSERT IGNORE INTO types VALUES (2, 'dog');
-INSERT IGNORE INTO types VALUES (3, 'lizard');
-INSERT IGNORE INTO types VALUES (4, 'snake');
-INSERT IGNORE INTO types VALUES (5, 'bird');
-INSERT IGNORE INTO types VALUES (6, 'hamster');
+INSERT IGNORE INTO types VALUES (1, '필라테스');
+INSERT IGNORE INTO types VALUES (2, '요가');
+INSERT IGNORE INTO types VALUES (3, '줌바댄스');
+INSERT IGNORE INTO types VALUES (4, '웨이트');
+INSERT IGNORE INTO types VALUES (5, '골프');
+INSERT IGNORE INTO types VALUES (6, '스피닝');
 
 INSERT IGNORE INTO owners VALUES (1, 'George', 'Franklin', '110 W. Liberty St.', 'Madison', '6085551023');
 INSERT IGNORE INTO owners VALUES (2, 'Betty', 'Davis', '638 Cardinal Ave.', 'Sun Prairie', '6085551749');
@@ -47,7 +47,7 @@ INSERT IGNORE INTO pets VALUES (11, 'Freddy', '2000-03-09', 5, 9);
 INSERT IGNORE INTO pets VALUES (12, 'Lucky', '2000-06-24', 2, 10);
 INSERT IGNORE INTO pets VALUES (13, 'Sly', '2002-06-08', 1, 10);
 
-INSERT IGNORE INTO visits VALUES (1, 7, '2010-03-04', 'rabies shot');
-INSERT IGNORE INTO visits VALUES (2, 8, '2011-03-04', 'rabies shot');
-INSERT IGNORE INTO visits VALUES (3, 8, '2009-06-04', 'neutered');
-INSERT IGNORE INTO visits VALUES (4, 7, '2008-09-04', 'spayed');
+INSERT IGNORE INTO visits VALUES (1, 7, '2010-03-04', '5회 남음');
+INSERT IGNORE INTO visits VALUES (2, 8, '2011-03-04', '6회 남음');
+INSERT IGNORE INTO visits VALUES (3, 8, '2009-06-04', '3회 남음');
+INSERT IGNORE INTO visits VALUES (4, 7, '2008-09-04', '2022-09-30까지');
